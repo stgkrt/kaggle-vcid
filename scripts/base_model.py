9f1670c74c5a43ba9c9b2f11d7f0067f06c889a9
stgkrt <taro.stst@gmail.com>
2023-04-03 23:08:55 +0000

    add segmodel using timm

--- scripts/base_model.py
+++ scripts/base_model.py
@@ -19,7 +19,7 @@ from torchinfo import summary
 class Encoder(nn.Module):
     def __init__(self, CFG):
         super().__init__()
-        self.encoder = timm.create_model(CFG["model_name"], in_chans=CFG["inp_channels"], features_only=True, out_indices=[0,1,2,3], pretrained=CFG["pretrained"])
+        self.encoder = timm.create_model(CFG["model_name"], in_chans=CFG["inp_channels"], features_only=True, out_indices=CFG["out_indices"], pretrained=CFG["pretrained"])
     def forward(self, img):
         skip_connection_list = self.encoder(img)
         return skip_connection_list
@@ -39,42 +39,76 @@ class UpConv(nn.Module):
         x = self.bn2(x)
         return x
 
+
 class Decoder(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super().__init__():
+    def __init__(self):
+        super().__init__()
         """
         encoderから320channelsで来たら、upconvしたものと合わせると、
         倍になるのでそれをchannelの入力にする？？
         """
-        self.UpConv_1 = UpConv(320, 160)
-        self.UpConv_2 = UpConv(160, 80)
-        self.UpConv_3 = UpConv(80, 20)
-        self.UpConv_5 = UpConv(24, 16)
+        channel_nums = [320, 112, 40, 24, 16]
+        self.UpConv_0 = UpConv(channel_nums[0], channel_nums[1])
+        self.UpConv_1 = UpConv(channel_nums[1]*2, channel_nums[2])
+        self.UpConv_2 = UpConv(channel_nums[2]*2, channel_nums[3])
+        self.UpConv_3 = UpConv(channel_nums[3]*2, channel_nums[4])
     
     def forward(self, skip_connection_list):
-        emb = self.UpConv_1(skip_connection_list[3]) # emb.shape = (None, 160, 14, 14)
-        print("1st emb", emb.shape)
+        [print("skip connetction shape", skc.shape) for skc in skip_connection_list]
+        emb = self.UpConv_0(skip_connection_list[4]) # emb.shape = (None, 160, 14, 14)
+        emb_cat = torch.cat([skip_connection_list[3], emb], dim = 1)
+        print("0 emb", emb.shape)
+        print("0 emb cat", emb_cat.shape)
+        
+        emb = self.UpConv_1(emb_cat)
         emb_cat = torch.cat([skip_connection_list[2], emb], dim = 1)
+        print("1 emb", emb.shape)
+        print("1 emb cat", emb_cat.shape)
+        
         emb = self.UpConv_2(emb_cat)
-        emb_cat = torch.cat([])
+        emb_cat = torch.cat([skip_connection_list[1], emb], dim = 1)
+        print("2 emb", emb.shape)
+        print("2 emb cat", emb_cat.shape)
+        
+        emb = self.UpConv_3(emb_cat)
+        emb_cat = torch.cat([skip_connection_list[0], emb], dim = 1)
+        print("3 emb", emb.shape)
+        print("3 emb cat", emb_cat.shape)
+        
+        return emb_cat
 
-
+class SegModel(nn.Module):
+    def __init__(self, CFG):
+        super().__init__()
+        self.encoder = Encoder(CFG)
+        self.decoder = Decoder()
+        self.head = nn.Sequential(
+            nn.Conv2d(32, CFG["out_channels"], kernel_size=1, stride=1, padding=0),
+            nn.Sigmoid()
+        )
+    def forward(self, img):
+        skip_connection_list = self.encoder(img)
+        emb = self.decoder(skip_connection_list)
+        output = self.head(emb)
+        return output
 
 if __name__=="__main__":
     CFG = {
         "model_name" : "tf_efficientnet_b0",
         "inp_channels" : 3,
-        "num_output" : 1,
-        "pretrained" : True
+        "out_channels" : 1,
+        "pretrained" : True,
+        "out_indices" : [0,1,2,3,4],
+        "batch_size" : 1,
     }
-    model = NFLNet(CFG)
-    # summary(
-    #     model,
-    #     input_size=(batch_size, 3, 224, 224),
-    #     col_names=["output_size", "num_params"],
-    #     depth=5,
-    # )
-    # feat_ext = create_feature_extractor(model, {"Conv2d":"skip_connection_1"})
+
+    model = SegModel(CFG)
     x = torch.rand((1, 3, 224, 224)).float()
     out_feat = model(x)
-    [print(o.shape) for o in out_feat]
+    print(out_feat.shape)
+    summary(
+        model,
+        input_size=(CFG["batch_size"], 3, 224, 224),
+        col_names=["output_size", "num_params"],
+        depth=5,
+    )
