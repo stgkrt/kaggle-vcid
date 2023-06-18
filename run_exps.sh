0dc631a7be6e45e06c760a6ccada021950006396
stgkrt <taro.stst@gmail.com>
2023-06-18 19:17:48 +0900

    end competition

--- run_exps.sh
+++ run_exps.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-python src/vcid_exp095.py
-python src/vcid_exp097.py
+# python src/vcid_exp095.py
+# python src/vcid_exp097.py
 
 # python src/vcid_1dexp001.py
 # python src/vcid_1dexp002.py
 
 # python src/vcid_1dexp003.py
 # python src/vcid_1dexp004.py
+
+python src/vcid_stack_exp002.py
+python src/vcid_stack_exp003.py
